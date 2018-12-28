43bb26453ee6ca39fd54b8dda24d8c685cb0dea7
David Stanley <davearthur.stanley@gmail.com>
2018-12-28 15:56:54 -0500

    Added .gitignore and other scripts from prev

---
+++ script_trim_foldernames.m
@@ -0,0 +1,42 @@
+
+
+%% Rename hcurrent6 folders
+
+do_git_mv = false;
+dry_run = false;
+% func_trim_foldernames('d180207_t032136__hcurrent6',do_git_mv,dry_run);
+
+%% Rename IB PPStim folders
+
+func_trim_foldernames('d180216_t231011__IBPPStim1',do_git_mv,dry_run);
+func_trim_foldernames('d180217_t170004__IBPPStim2',do_git_mv,dry_run);
+func_trim_foldernames('d180217_t180619__IBPPStim3',do_git_mv,dry_run);
+func_trim_foldernames('d180217_t223539__IBPPStim4',do_git_mv,dry_run);
+func_trim_foldernames('d180217_t232032__IBPPStim5',do_git_mv,dry_run);
+func_trim_foldernames('d180218_t000748__IBPPStim6',do_git_mv,dry_run);
+func_trim_foldernames('d180218_t004620__IBPPStim7',do_git_mv,dry_run);
+
+%% Rename next hcurrent6 folder...
+func_trim_foldernames('d180321_t155456__hcurrent6_default',do_git_mv,dry_run);
+
+
+
+%% Rename next PPfreqSweep_v1.3 folder...
+func_trim_foldernames('d181209_t200609__PPfreqSweep_v1.3',do_git_mv,dry_run);
+
+
+
+%% Rename next Model_IBPPStim7_v7.1 folder...
+func_trim_foldernames('d181210_t124817__Model_IBPPStim7_v7.1',do_git_mv,dry_run);
+
+
+
+
+%% Rename next Model_hrw_v1.3 folder...
+currd = pwd;
+cd ../model-dnsim-kramer_IB_Figs/
+func_trim_foldernames('d181219_t172155__hcurrent7k_poissTau_1.0',do_git_mv,dry_run);
+cd (currd)
+
+
+
